7a5f3e8ede634815ba08fd427826fd6d39f86139
iamkuldeeppanwar <kuldeep10panwar@gmail.com>
2021-12-24 21:07:11 +0530

    third commit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-require("dotenv").config();
 const cors = require("cors");
+require("dotenv").config();
 const express = require("express");
 require("./mongodb/mongoose");
 const userRouter = require("./router/user");
@@ -8,13 +8,11 @@ const cookieParser = require("cookie-parser");
 const path = require("path");
 
 const app = express();
-
+const port = process.env.PORT || 4000;
 app.use(cors());
+
 app.use("/public/img/user", express.static(path.join("public", "img", "user")));
 app.use("/public/img/blog", express.static(path.join("public", "img", "blog")));
-const port = process.env.PORT || 4000;
-
-app.use(express.json({ limit: "50mb" }));
 app.use(cookieParser());
 app.use(userRouter);
 app.use(blogRouter);
