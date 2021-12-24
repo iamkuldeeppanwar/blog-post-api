7cc4761f498c9b10466554b24209b3a4482cc132
iamkuldeeppanwar <kuldeep10panwar@gmail.com>
2021-12-24 20:23:45 +0530

    second commit

--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
 require("dotenv").config();
+const cors = require("cors");
 const express = require("express");
 require("./mongodb/mongoose");
-const cors = require("cors");
 const userRouter = require("./router/user");
 const blogRouter = require("./router/blog");
 const cookieParser = require("cookie-parser");
 const path = require("path");
 
 const app = express();
+
+app.use(cors());
 app.use("/public/img/user", express.static(path.join("public", "img", "user")));
 app.use("/public/img/blog", express.static(path.join("public", "img", "blog")));
 const port = process.env.PORT || 4000;
 
-app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 app.use(cookieParser());
 app.use(userRouter);
